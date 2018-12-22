3b5e99ba6356f62bd62da1e0bcf97b46e183d9ed
Joel Gibson <joel@jgibson.id.au>
2018-12-22 12:57:12 +1100

    Update for slack

--- lolbot.py
+++ lolbot.py
@@ -5,12 +5,18 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # You can message lol_bot via <your website>/lol
-@app.route('/lol')
+#@app.route('/lol')
+@app.route('/lol', methods=['POST'])
 def lol_bot():
     # Get the value of the 'text' query parameter
     # request.args is a dictionary (cool!)
-    text = request.args.get('text')
+    #text = request.args.get('text')
     # This bot lols at every command it gets sent!
+    #return f'lol {text}'
+
+    # Get the value of the 'text' query parameter
+    # request.form is a dictionary (cool!)
+    text = request.form.get('text')
     return f'lol {text}'
 
 
